7dd478cf2ac72b76ff875215ea86819e7db94e3c
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-31 17:23:50 -0500

    Metodos del repositorio para comunicarse con mongoDB

--- src/main/java/co/com/sofka/questions/repositories/QuestionRepository.java
+++ src/main/java/co/com/sofka/questions/repositories/QuestionRepository.java
@@ -1,11 +1,14 @@
-package co.com.sofka.questions.reposioties;
+package co.com.sofka.questions.repositories;
 
 import co.com.sofka.questions.collections.Question;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface QuestionRepository extends ReactiveCrudRepository<Question, String> {
     Flux<Question> findByUserId(String userId);
+    Mono<Question> findByIdAndUserId(String id, String userId);
+    Flux<Question> findByQuestionLike(String question);
 }
